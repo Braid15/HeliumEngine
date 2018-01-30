dd95f48690d786bf7ab91d0776d826d007ae4ac7
Roy Atkins <royanthony0522@gmail.com>
2018-01-29 19:27:36 -0500

    Initial commit

---
+++ src/helium_std.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "targetver.h"
+
+#ifndef _UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+#include <windowsx.h>
+
+#include <tchar.h>
+#include <malloc.h> 
+#include <memory.h>
+#include <io.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <array>
+#include <vector>
+#include <math.h>
+#include <list>
+#include <filesystem>
+#include <typeinfo>
+#include <type_traits>
+#include <iomanip>
+#include <cassert>
+#include <strstream>
+
+#include <core/types.h>
